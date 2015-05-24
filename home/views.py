6c0b456358175b80fe9d918e61b37015a23be0b9
wreckoner <dibyendudas.1990@gmail.com>
2015-05-24 12:15:26 -0400

    Changed display date to javascript to match clients local time.

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ import random
 
 def home(requests):
 	template = "home.html"
-	context = {'title':'Dibyendu Das', 'datetime':timezone.now()}
+	context = {'title':'Dibyendu Das'}
 	context['heading'] = 'Dibyendu Das'
 	context['description'] = "Welcome to my website. You can click on the links above for more information!"
 	context['work'] = [{'head':x.heading, 'desc':x.description, 'url':x.url, 'start':x.start, 'stop':x.stop} for x in db.objects.filter(category='WR')]
