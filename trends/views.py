9aeec3c129e808a8312a37e697f6a1afb64705e7
wreckoner <dibyendudas.1990@gmail.com>
2015-05-26 12:31:35 -0400

    Added traceback to error message.

--- trends/views.py
+++ trends/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from django.utils import timezone, dateparse
 from datetime import timedelta
 from .models import TwitterToken, TwitterTrend
-import tweepy
+import tweepy, traceback
 
 # Create your views here.
 def trends(requests):
@@ -29,7 +29,7 @@ def trends(requests):
 			TwitterTrend(location_id=data['locations'][0]['woeid'], trends=data, location=data['locations'][0]['name'], created_at=dateparse.parse_datetime(data['created_at'])).save()
 			context[u'twitter_status'] = u'Retrieved trends from Twitter API and updated database.'
 		except Exception, e:
-			context[u'twitter'] = str(e)
+			context[u'twitter'] = '. '.join([str(e), traceback.format_exc()])
 			context[u'twitter_status'] = u'Failed to retrieve trends.'			
 	else:
 		context[u'twitter'] = TwitterTrend.objects.filter(location_id=1)[0].trends
