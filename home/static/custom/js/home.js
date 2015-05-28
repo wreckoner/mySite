c6b05dbbad8f53d20c3bc3d1d9ed37b7155a5176
wreckoner <dibyendudas.1990@gmail.com>
2015-05-28 00:09:46 -0400

    Addded fading to home page! Reversed order by.

--- home/static/custom/js/home.js
+++ home/static/custom/js/home.js
@@ -1,14 +1,17 @@
 function menuSelect (argument) {
-	$(".jumbotron p").empty();
+	
+	$(".jumbotron p").hide(
+		function() {
+				$(this).text(
+			function(){ 
+				if (argument == ".prosearch")
+					{return "Projects and Research";}
+				else if(argument == ".work")
+					{return "Work";}
+				else if (argument == ".education")
+					{return "Education";}
+			}).fadeIn();
+		});
 	$(".row").hide();
-	$(".jumbotron p").text(
-		function(){ 
-			if (argument == ".prosearch")
-				{return "Projects and Research";}
-			else if(argument == ".work")
-				{return "Work";}
-			else if (argument == ".education")
-				{return "Education";}
-		}).fadeIn();
 	$(argument).fadeToggle();
 }
--- home/views.py
+++ home/views.py
@@ -11,9 +11,9 @@ def home(requests):
 	context = {'title':'Dibyendu Das'}
 	context['heading'] = 'Dibyendu Das'
 	context['description'] = "Welcome to my website. You can click on the links above for more information!"
-	context['work'] = [{'head':x.heading, 'desc':x.description, 'url':x.url, 'start':x.start, 'stop':x.stop} for x in db.objects.filter(category='WR').order_by('stop')]
-	context['education'] = [{'head':x.heading, 'desc':x.description, 'url':x.url, 'start':x.start, 'stop':x.stop} for x in db.objects.filter(category='ED').order_by('stop')]
-	context['prosearch'] = [{'head':x.heading, 'desc':x.description, 'url':x.url, 'start':x.start, 'stop':x.stop} for x in db.objects.filter(category='PR').order_by('stop')]
+	context['work'] = [{'head':x.heading, 'desc':x.description, 'url':x.url, 'start':x.start, 'stop':x.stop} for x in db.objects.filter(category='WR').order_by('stop').reverse()]
+	context['education'] = [{'head':x.heading, 'desc':x.description, 'url':x.url, 'start':x.start, 'stop':x.stop} for x in db.objects.filter(category='ED').order_by('stop').reverse()]
+	context['prosearch'] = [{'head':x.heading, 'desc':x.description, 'url':x.url, 'start':x.start, 'stop':x.stop} for x in db.objects.filter(category='PR').order_by('stop').reverse()]
 	background_colors = ['darkolivegreen', 'darkslategray', 'firebrick', 'indigo', 'slategray', 'olivedrab', 'teal', 'black', 'steelblue', 'forestgreen', 'darkgoldenrod', 'mediumorchid', 'darkslateblue', 'indianred', 'cornflowerblue']
 	context['bgColor'] = random.choice(background_colors)
 	return render(requests, template, context)
